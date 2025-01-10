5d2a140a259aed36686100175102dea84881b4ab
Daniel Zoba <daniel.sobe@sorben.com>
2025-01-10 15:08:03 +0100

    fix port visibility outside container

--- serbske_modele/server/startme.sh
+++ serbske_modele/server/startme.sh
@@ -3,5 +3,5 @@
 echo "Using first model found:"
 ls -l /model/
 
-/llama.cpp_b4458/build/bin/llama-server --model /model/*.gguf
+/llama.cpp_b4458/build/bin/llama-server --model /model/*.gguf --host 0.0.0.0 --port 8080
 
