b1a4171a84de89c2abff9a45a83d2b53b1e2e01d
Daniel Zoba <daniel.sobe@sorben.com>
2026-01-29 09:26:25 +0100

    update llama.cpp usage for server app

--- serbske_modele/server/startme.sh
+++ serbske_modele/server/startme.sh
@@ -5,5 +5,5 @@ nvidia-smi
 echo "Using first model found:"
 ls -l /model/
 
-/llama.cpp_b4458/build/bin/llama-server --model /model/*.gguf --host 0.0.0.0 --port 8080
+/llama.cpp_b7852/build/bin/llama-server --model /model/*.gguf --host 0.0.0.0 --port 8080
 
